8e6bbe8da4b48c394b6a05bc35a1cc35dc4422fe
Greg Doud <gpdoud@gmail.com>
2024-06-14 13:43:53 -0400

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp4;
+
+internal class Program {
+
+    static void Main( string[] args) {
+
+        TQL tql = new TQL { SecredNumber = 1 };
+        tql.SecredNumber = 1;
+        tql.DisplayClassname();
+        var newSecretNumber = tql.RandomizeSecretNumber(321);
+        Console.WriteLine($"newSecretNumber is {newSecretNumber}");
+        tql.DisplayClassname();
+    }
+}
---
+++ ConsoleApp4/TQL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4;
+
+internal class TQL {
+
+    public int SecredNumber { get; set; }
+
+    public void DisplayClassname() {
+        Console.WriteLine($"Classname is TQL; SecretNumber is {SecredNumber}");
+    }
+
+    public int RandomizeSecretNumber(int RandomNumber) {
+        SecredNumber *= RandomNumber;
+        return SecredNumber;
+    }
+
+}
